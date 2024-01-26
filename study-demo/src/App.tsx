04186febf0673cec18107acb827a5dd4dedaeb14
Waker <1209562577@qq.com>
2024-01-26 15:53:37 +0800

    🔧 chore <husky>

--- study-demo/src/App.tsx
+++ study-demo/src/App.tsx
@@ -11,6 +11,7 @@ function App() {
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
+          <div>test</div>
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
